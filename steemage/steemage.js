a1a9d6840b3ad355a4048ba7dafba66585a011fb
revo-lutionist <evolutionist.r@gmail.com>
2018-07-09 18:09:02 +1000

    extract images from users blog via the blockchain output them onto a single page

---
+++ steemage/steemage.js
@@ -0,0 +1,55 @@
+var mCount = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    //show loader gif
+    document.getElementById("loader").style.display = "block";
+});
+
+//listen for message from background.js
+chrome.runtime.onMessage.addListener(
+    function(request) {
+      if (request.message == "ready")
+        document.getElementById("loader").style.display = "none";
+        outputImages();
+});
+
+async function outputImages() {
+    var arrImgObjects = JSON.parse(window.localStorage.getItem("images"));
+
+    for (var objImg of arrImgObjects) {
+        //correct known parsing issue (there may be more to be discovered)
+        objImg.image = objImg.image.replace(/\(/g, "\\(");
+        objImg.image = objImg.image.replace(/\)/g, "\\)");
+
+        //exclude gifs, as they are more likely to be fillers
+        if(!(objImg.image.includes(".gif") || objImg.image.includes(".GIF"))) {
+            createImage(objImg);
+        }
+    }
+}
+
+function createImage(objImg) {
+    mCount++;
+    
+    //create image and container elements and append to existing column divs in steemage.html
+    var divOuterContainer = document.createElement("div");
+    var anchor = document.createElement("a");
+    var img = document.createElement("img");
+    img.setAttribute("src", objImg.image);
+
+    anchor.appendChild(img);
+    anchor.setAttribute("href", objImg.image);
+    anchor.setAttribute("target", "_blank");
+
+    divOuterContainer.appendChild(anchor);
+    document.getElementById(mCount.toString()).appendChild(divOuterContainer);  //this is a column
+
+    divOuterContainer.classList.add("outerContainer");
+
+    //3 columns only
+    if (mCount == 3) {
+        mCount = 0;
+    }
+}
+
+
